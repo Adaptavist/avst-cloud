4f46738324abc9cfc123f3608936f7685b7e2e95
Martin Brehovsky <mbrehovsky@adaptavist.com>
2016-02-15 16:55:49 +0100

    Added created by to aws tags

--- lib/avst-cloud/aws_connection.rb
+++ lib/avst-cloud/aws_connection.rb
@@ -33,7 +33,7 @@ module AvstCloud
             AvstCloud::AwsServer.new(server, server_name, server.public_ip_address, root_user, root_password)
         end
 
-        def create_server(server_name, flavour, os, key_name, ssh_key, subnet_id, security_group_ids, ebs_size, hdd_device_path, ami_image_id, availability_zone, vpc=nil)
+        def create_server(server_name, flavour, os, key_name, ssh_key, subnet_id, security_group_ids, ebs_size, hdd_device_path, ami_image_id, availability_zone, vpc=nil, created_by=nil)
 
             # Permit named instances from DEFAULT_FLAVOURS
             flavour = flavour || "t2.micro"
@@ -98,12 +98,15 @@ module AvstCloud
                                             } 
                                         ]
                 end
-
+                tags = {
+                    'Name' => server_name,
+                    'os' => os
+                }
+                if created_by 
+                    tags['created_by'] = created_by
+                end
                 # create server
-                server = connect.servers.create :tags => {
-                                                    'Name' => server_name,
-                                                    'os' => os
-                                                },
+                server = connect.servers.create :tags => tags,
                                                 :flavor_id => flavour,
                                                 :image_id => ami_image_id,
                                                 :key_name => key_name,
